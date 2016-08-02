fa6721a2f940922f03de1591c59d3e363f5bfff5
zkocen <zkocen@gmail.com>
2016-08-02 19:56:37 +0100

    fin

--- src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -22,7 +22,7 @@ public class FamilyMembersActivity extends AppCompatActivity {
         famMem.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
         famMem.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
         famMem.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
-        famMem.add(new Word("daughter", "tune", R.drawable.family_daughter, R.drawable.family_daughter));
+        famMem.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
         famMem.add(new Word("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
         famMem.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
         famMem.add(new Word("older sister", "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
@@ -31,14 +31,14 @@ public class FamilyMembersActivity extends AppCompatActivity {
         famMem.add(new Word("grandfather", "paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
 
         WordAdapter adapter = new WordAdapter(this, famMem, R.color.category_family);
-        ListView listView = (ListView)findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Word famMems = famMem.get(position);
-                mediaPlayer.create(FamilyMembersActivity.this, famMems.getmAudioResourceID());
+                Word familia = famMem.get(position);
+                mediaPlayer = mediaPlayer.create(FamilyMembersActivity.this, familia.getmAudioResourceID());
                 mediaPlayer.start();
             }
         });
